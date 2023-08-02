ed01b12fd626dc1165e527bfa9f4351dd7393b0f
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-08-02 13:29:21 +0530

    formatting

--- LAB_1/program_1b.c
+++ LAB_1/program_1b.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-void main(){
-    float a,b;
-    
+void main()
+{
+    float a, b;
+
     float c;
 
     printf("enter first number :");
-    scanf("%f",&a);
+    scanf("%f", &a);
     printf("enter second number :");
-    scanf("%f",&b);
-    printf("sum is %f \n",a+b);
-    printf("subtract is  %f \n",a-b);
-    printf("multiplication  %f \n",a*b);
-    c=(float)a/b;
+    scanf("%f", &b);
+    printf("sum is %f \n", a + b);
+    printf("subtract is  %f \n", a - b);
+    printf("multiplication  %f \n", a * b);
+    c = (float)a / b;
 
     printf("divide is %f", c);
 }
--- LAB_1/program_2.c
+++ LAB_1/program_2.c
@@ -1,41 +1,22 @@
 #include <stdio.h>
 
-void main(){
+void main()
+{
     float salary;
-    float  da,hra, m1,m2,m3;
+    float da, hra, m1, m2, m3;
     float gross_salary;
 
-
-
-
-   
-
-
     printf("enter the basic salary  ");
-    scanf("%f",&salary);
+    scanf("%f", &salary);
     printf("enter the  daily allowence in percentage ");
-    scanf("%f",&da);
+    scanf("%f", &da);
     printf("enter the  house rent  allowence in percentage  ");
-    scanf("%f",&hra);
+    scanf("%f", &hra);
 
-    m3=(da/100.0);
-    m1=m3*salary;
+    m3 = (da / 100.0);
+    m1 = m3 * salary;
 
-    
-
-
-    m2=(float) (hra/100)*salary;
-    gross_salary= (float) salary+m1+m2;
-    printf("total salary is %f",gross_salary);
-
-
-
-
-
-
-
-   
-
-
-
+    m2 = (float)(hra / 100) * salary;
+    gross_salary = (float)salary + m1 + m2;
+    printf("total salary is %f", gross_salary);
 }
