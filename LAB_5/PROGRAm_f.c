e20744e50f28ccde8f2fb97dd5faf7f9056ece7c
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-08-02 13:31:40 +0530

    formatting

--- LAB_5/PROGRAm_f.c
+++ LAB_5/PROGRAm_f.c
@@ -1,39 +1,37 @@
-#include<stdio.h>
-void main(){
-    int a,b;
+#include <stdio.h>
+void main()
+{
+    int a, b;
     printf("enter the first number:");
-    scanf("%i",&a);
+    scanf("%i", &a);
     printf("Enter the second number:");
-    scanf("%i",&b);
-    int sum=0;
+    scanf("%i", &b);
+    int sum = 0;
 
-    for(int f=a;f<b+1;f++){
-        if(f!=1){
-            for(int d=2;d<f;d++){
-                if(f%d==0){
+    for (int f = a; f < b + 1; f++)
+    {
+        if (f != 1)
+        {
+            for (int d = 2; d < f; d++)
+            {
+                if (f % d == 0)
+                {
                     sum++;
-                    
-
-
-
                 }
-                
             }
-            if(sum==0){
-                printf("%i is prime number\n",f);
+            if (sum == 0)
+            {
+                printf("%i is prime number\n", f);
             }
-            else{
-                printf("%i is a composite number\n",f);
-
+            else
+            {
+                printf("%i is a composite number\n", f);
             }
-            sum=0;
-            
-
+            sum = 0;
         }
-        else{
+        else
+        {
             printf("1 is a prime number\n");
         }
-
-
     }
-}    
+}
--- LAB_5/program_2_2.c
+++ LAB_5/program_2_2.c
@@ -1,20 +1,22 @@
-#include<stdio.h>
-void main(){
-    for(int y=1;y<7;y++){
-        for(int f=0;f<6-y;f++){
+#include <stdio.h>
+void main()
+{
+    for (int y = 1; y < 7; y++)
+    {
+        for (int f = 0; f < 6 - y; f++)
+        {
             printf(" ");
-
         }
-        for(int b=1;b<y+1;b++){
-            printf("%i",b);
+        for (int b = 1; b < y + 1; b++)
+        {
+            printf("%i", b);
         }
-        for(int h=y-1;h>0;h--){
+        for (int h = y - 1; h > 0; h--)
+        {
 
-            printf("%i",h);
-
+            printf("%i", h);
+            printf("%i", h);
         }
         printf("\n");
-
-    
     }
 }
