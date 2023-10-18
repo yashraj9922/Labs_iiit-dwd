14f01100f2b50bb144557794ecd6bff0b14494a9
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-10-18 15:46:06 +0530

    .

--- Semester-3/DAA/Lab-6/BinaryTreeUsingStack.java
+++ Semester-3/DAA/Lab-6/BinaryTreeUsingStack.java
@@ -10,7 +10,7 @@ class Node {
 	}
 }
 
-class BinaryTree {
+class BinaryTreeUsingStack {
 	Node root;
 
 	void inorder() {
@@ -38,7 +38,7 @@ class BinaryTree {
 
 	public static void main(String args[]) {
 
-		BinaryTree tree = new BinaryTree();
+		BinaryTreeUsingStack tree = new BinaryTreeUsingStack();
 		tree.root = new Node(1);
 		tree.root.left = new Node(2);
 		tree.root.right = new Node(3);
