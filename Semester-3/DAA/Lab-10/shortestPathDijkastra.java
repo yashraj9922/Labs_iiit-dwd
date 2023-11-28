fa411a51b0c6d16ad1c1d8493f8c7629eae14791
Yashraj Kadam <133231190+yashraj9922@users.noreply.github.com>
2023-11-28 23:08:17 +0530

    Update shortestPathDijkastra.java

--- Semester-3/DAA/Lab-10/shortestPathDijkastra.java
+++ Semester-3/DAA/Lab-10/shortestPathDijkastra.java
@@ -1,9 +1,7 @@
 public class shortestPathDijkastra {
-    // A Java program for Dijkstra's single source shortest path algorithm.
     static final int V = 9;
 
     int minDistance(int dist[], Boolean sptSet[]) {
-        // Initialize min value
         int min = Integer.MAX_VALUE, min_index = -1;
 
         for (int v = 0; v < V; v++)
@@ -31,34 +29,20 @@ public class shortestPathDijkastra {
             sptSet[i] = false;
         }
 
-        // Distance of source vertex from itself is always 0
         dist[src] = 0;
 
-        // Find shortest path for all vertices
         for (int count = 0; count < V - 1; count++) {
-            // Pick the minimum distance vertex from the set
-            // of vertices not yet processed. u is always
-            // equal to src in first iteration.
             int u = minDistance(dist, sptSet);
 
-            // Mark the picked vertex as processed
             sptSet[u] = true;
 
-            // Update dist value of the adjacent vertices of
-            // the picked vertex.
             for (int v = 0; v < V; v++)
-
-                // Update dist[v] only if is not in sptSet,
-                // there is an edge from u to v, and total
-                // weight of path from src to v through u is
-                // smaller than current value of dist[v]
                 if (!sptSet[v] && graph[u][v] != 0
                         && dist[u] != Integer.MAX_VALUE
                         && dist[u] + graph[u][v] < dist[v])
                     dist[v] = dist[u] + graph[u][v];
         }
 
-        // print the constructed distance array
         printSolution(dist);
     }
 
